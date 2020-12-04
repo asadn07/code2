733024c20d8625ee6a662ebe4db92731371e6a99
asadn07 <75382100+asadn07@users.noreply.github.com>
2020-12-04 19:19:09 +0100

    Update a2.c

--- a2.c
+++ a2.c
@@ -1,7 +1,8 @@
 #include "framework.h"
 #include <stdio.h>
 
-int getOption() {
+int getOption()
+{
     printf("\nWählen Sie bitte eine Option:\n"
            "1 - Verschlüsseln\n"
            "2 - Entschlüsseln\n"
@@ -10,7 +11,8 @@ int getOption() {
     int option = 0;
     scanf("%d ", &option);
 
-    switch (option) {
+    switch(option)
+    {
         case 0:
             return 0;
         case 1:
@@ -18,45 +20,21 @@ int getOption() {
         case 2:
             return 2;
         default:
-            return getOption();
+            return option;
     }
 }
 
-int go_right(int x) {
-    if (x % 5 == 4) {
-        return x - 4;
-    } else {
-        return x + 1;
-    }
-}
-
-void encrypt(char *key, char left_letter, char right_letter,
-             char *result_left, char *result_right) {
-    int left_letter_location, right_letter_location;
-    for (int i = 0; i < 25; i++) {
-        if (key[i] == left_letter) {
-            left_letter_location = i;
-        } else if (key[i] == right_letter) {
-            right_letter_location = i;
-        }
-    }
-
-    // They are on the same row
-    if (left_letter_location / 5 == right_letter_location / 5) {
-        *result_left = key[go_right(left_letter_location)];
-        *result_right = key[go_right(right_letter_location)];
-    } else if (left_letter_location % 5 == right_letter_location % 5) {
-
-    }
-}
-
-int process_input(int option, char *key) {
+int encrypt(char* playfair)
+{
+    printf("Klartext: ");
     char input[100];
     char c;
     int size_input = 0;
-    for (int i = 0; i < 50; i++) {
+    for(int i = 0; i < 50; i++)
+    {
         c = getchar();
-        if (c == '\0' || c == '\n') {
+        if(c == '\0' || c =='\n')
+        {
             input[i] = '\0';
             break;
         }
@@ -65,97 +43,152 @@ int process_input(int option, char *key) {
     }
     removeSpaces(input);
     toUpper(input);
-    if (option == 1) { replaceLetters(input, 'W', 'V'); } // nur bei verschlüsseln
-    if (size_input > 50) {
+    replaceLetters(input,'W','V');
+
+
+    if(size_input > 50)
+    {
         printf("zu groß !, return 0; TODO: CHANGE TO CORRECT ERRORMSG!");
         return -1;
     }
-    
-//    int i = 0;
-//    while (i < stringLength(input)) {
-//        if (input[i] == input[i + 1]) {
-//            for (int k = stringLength(input) + 1; k > i; k--) { input[k] = input[k - 1]; };
-//            input[i + 1] = 'X';
-//        }
-//        i++;
-//    }
-
-    if (size_input > 50) {
-        printf("zu groß !, return 0; TODO: CHANGE TO CORRECT ERRORMSG!");
-        return -1;
-    }
-
-    int input_length = stringLength(input);
-    char bigram[input_length][2];
-    char cipher_text[input_length][2];
-    int bigram_id = 0;
-    for (int j = 0; j < input_length; j++) {
-        if (j != input_length - 1 && input[j] != input[j + 1]) {
-            bigram[bigram_id][0] = input[j];
-            bigram[bigram_id][1] = input[j + 1];
-            j++;
-        } else {
-            bigram[bigram_id][0] = input[j];
-            bigram[bigram_id][1] = 'X';
+    int i = 0;
+    while(i < stringLength(input))
+    {
+        if(input[i] == input[i+1])
+        {
+            for (int k = stringLength(input) + 1; k > i; k--)
+            {input[k] = input[k - 1];};
+            input[i+1] = 'X';
         }
-        char encrypted_left, encrypted_right;
-        encrypt(key, bigram[bigram_id][0], bigram[bigram_id][1],
-                &encrypted_left, &encrypted_right);
-        cipher_text[bigram_id][0] = encrypted_left;
-        cipher_text[bigram_id][1] = encrypted_right;
-
-        printf("%c%c ", encrypted_left, encrypted_right);
-
-        bigram_id++;
+        i++;
     }
-    //TODO: HERE BIGRAM IST FERTIG
-    // MIT OPTION KANN MAN ENTSCHEIDEN OB VER ODER ENTSCHLÜSSELN
-    // JEW EINE FUNKT  SCHREIBEN DIE DAS MACHT
-    // DA GIBT ES REGELN IN DER ANGABE ... BIGRAMM IST EIN 2D ARRAY.. MUSST DU RICHTIG DURCHITERIEREN
 
-    printf("\n option ist %d\n", option);
+    if(size_input > 50)
+    { printf("zu groß !, return 0; TODO: CHANGE TO CORRECT ERRORMSG!");
+        return -1;
+    }
 
+    if((stringLength(input)) % 2 == 1)
+    { // ungerade X hinzufügen!
+        input[stringLength(input)] = 'X';
+    }
+
+    char bigram[(stringLength(input))/2][2];
+
+    int pos_in_input = 0 ;
+
+    for(int j = 0; j < stringLength(input) / 2; j++)
+    {
+        for(int k = 0; k < 2; k++)
+        {
+            bigram[j][k] = input[pos_in_input];
+            pos_in_input++;
+        }
+    }
+
+    printf("Vorbereiteter Klartext: ");
+    for(int j = 0; j < stringLength(input) / 2; j++)
+    {
+        for(int k = 0; k < 2; k++)
+        {
+            printf("%c",bigram[j][k]);
+        }
+        printf(" ");
+    }
+
+    pos_in_input = 0;
+    int pos_bi1 = 0;
+    int pos_bi2 = 0;
+    for(int j = 0; j < stringLength(input) / 2; j++)
+    {
+        while (pos_in_input < 25)
+        {
+            if (playfair[pos_in_input] == bigram[j][0])
+            {
+                pos_bi1 = pos_in_input;
+            }
+            if (playfair[pos_in_input] == bigram[j][1])
+            {
+                pos_bi2 = pos_in_input;
+            }
+            pos_in_input++;
+        }
+        pos_in_input = 0;
+        //selbe spalte
+        if(pos_bi1 - pos_bi2 == 5 || pos_bi2 - pos_bi1 == 5)
+        {
+            if(pos_bi1 + 5 < 25)
+            {
+                bigram[j][0] = playfair[pos_bi1+5];
+            }else
+                {
+                bigram[j][0] = playfair[pos_bi1-20];
+            }
+            if(pos_bi2 + 5 < 25)
+            {
+                bigram[j][1] = playfair[pos_bi2+5];
+            }
+            else
+                {
+                bigram[j][1] = playfair[pos_bi2-20];
+            }
+        }
+    }
+
+    printf("\nCiphertext: ");
+    for(int j = 0; j < stringLength(input) / 2; j++)
+    {
+        for(int k = 0; k < 2; k++)
+        {
+           // bigram[j][1] = 66;
+            printf("%c", bigram[j][k]);
+        }
+        //printf(" ");
+    }
+    printf("\n");
 
     return 0;
 }
 
-
-int main() {
-
+int main()
+{
     char key[25];
     generatePlayfairSquare(key);
-
     //choose option
     int option = getOption();
-    if (option == 0) {
+    if(option == 0 )
+    {
         return 0;
     }
 
-    int retval = process_input(option, key);
-    if (retval == -1) { // meaning some kind of error
+    int retval = encrypt(key);
+
+    if(retval == -1)
+    { // meaning some kind of error
         printf("ERROR ODER irgendwas ANDERES!\n");
     }
+
     return 0;
 }
 
 //-----------------------------------------------------------------------------
-// Computes and returns the length of a string 
+// Computes and returns the length of a string
 //
-// @param text string to check 
+// @param text string to check
 //
 // @return length of the string
 //
 int stringLength(char *text);
 
 //-----------------------------------------------------------------------------
-// Changes a string to upper case 
+// Changes a string to upper case
 //
 // @param text string to modify
 //
 void toUpper(char *text);
 
 //-----------------------------------------------------------------------------
-// Replace all the occurences of a letter in a string with another letter 
+// Replace all the occurences of a letter in a string with another letter
 //
 // @param text string to modify
 // @param original letter to replace
@@ -165,47 +198,45 @@ void replaceLetters(char *text, char original, char new_char);
 
 
 //-----------------------------------------------------------------------------
-int stringLength(char *text) {
-    // TODO: Implement
+int stringLength(char *text)
+{
     int i = 0;
-    while (text[i] != '\0') {
+    while(text[i] != '\0')
+    {
         i++;
     }
-    // WARNING: Do not change the signature of the function
     return i;
+    // WARNING: Do not change the signature of the function
 }
 
 //-----------------------------------------------------------------------------
-void toUpper(char *text) {
-
-
+void toUpper(char *text)
+{
     int i = 0;
-    while (text[i] != '\0') {
-        if (text[i] >= 'a' && text[i] <= 'z') {
-            text[i] = text[i] - 32;
+    while(text[i] != '\0')
+    {
+        if(text[i] >= 'a' && text[i] <= 'z')
+        {
+            text[i] = text[i] -32;
         }
 
         i++;
     }
-
-    // TODO: Implement
-
-
     // WARNING: Do not change the signature of the function
 }
 
 //-----------------------------------------------------------------------------
-void replaceLetters(char *text, char original, char new_char) {
+void replaceLetters(char *text, char original, char new_char)
+{
 
     int i = 0;
-    while (text[i] != '\0') {
-        if (text[i] == original) {
+    while(text[i] != '\0')
+    {
+        if(text[i] == original)
+        {
             text[i] = new_char;
         }
-
         i++;
     }
-
-    // TODO: Implement
     // WARNING: Do not change the signature of the function
 }
